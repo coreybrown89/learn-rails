5d7779777de43e741a5fae508bca8388abe4ac9b
Corey Brown <coreybrown89@gmail.com>
2016-05-11 12:13:14 -0500

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,20 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.smtp_settings = {
+  address: "smtp.sendgrid.net",
+  port: 587,
+  domain: Rails.application.secrets.domain_name,
+  authentication: "plain",
+  user_name: Rails.application.secrets.email_provider_username,
+  password: Rails.application.secrets.email_provider_password
+}
+# ActionMailer Config
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.raise_delivery_errors = true
+# Send email in development mode?
+config.action_mailer.perform_deliveries = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
